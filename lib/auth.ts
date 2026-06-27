92f7d87791ff440f5eb9ec4719c80171f8cc854d
Federico Giorgino <fed.giorgino@proton.me>
2026-06-27 22:02:39 +0200

    fix: missing field from betterAuth config

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,8 @@ import { nextCookies } from "better-auth/next-js";
 import { db } from "../drizzle/db"; // your drizzle instance
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   emailAndPassword: {
     enabled: true,
   },
@@ -15,7 +17,7 @@ export const auth = betterAuth({
   },
   plugins: [nextCookies()],
   database: drizzleAdapter(db, {
-    provider: "pg", // or "mysql", "sqlite"
+    provider: "pg",
   }),
 });
 
